d6ca1f51abaf4b68601d2999ce826d0177e5ba5e
tuanhung0912 <tuanhung.bh2003@gmail.com>
2026-05-09 15:30:14 +0700

    Bookmarking Volumes to automatic Refreshing

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Welcome to ReactJS. Click here to learn more about ReactJS.
         </a>
       </header>
     </div>
